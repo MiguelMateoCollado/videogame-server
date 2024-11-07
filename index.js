9c1e8f42907f1ee500fa1bf4c68c7e6d2d06dc25
grupodcm <dcmdesarrollo@gmail.com>
2024-11-07 16:01:13 -0500

    cors

--- index.js
+++ index.js
@@ -16,14 +16,20 @@ server.use(
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credential", "true");
-  res.setHeader("Access-Control-Allow-Origin", [
+  const allowedOrigins = [
     "https://api.rawg.io/",
     "https://videogames.mmateo.com/",
-  ]);
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });

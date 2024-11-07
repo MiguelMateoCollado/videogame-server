9bfb948f876cfb06d3c1fcb59dd4edf249494e17
grupodcm <dcmdesarrollo@gmail.com>
2024-11-07 15:56:12 -0500

    exactly

--- index.js
+++ index.js
@@ -16,11 +16,10 @@ server.use(
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credential", "true");
-  res.setHeader(
-    "Access-Control-Allow-Origin",
+  res.setHeader("Access-Control-Allow-Origin", [
     "https://api.rawg.io/",
-    "https://videogames.mmateo.com/"
-  );
+    "https://videogames.mmateo.com/",
+  ]);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
